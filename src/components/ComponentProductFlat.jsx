2dcce7b477ff156e43e141fb2667ef0574eb42bd
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-09 21:09:22 -0300

    1.0

--- src/components/ComponentProductFlat.jsx
+++ src/components/ComponentProductFlat.jsx
@@ -1,20 +1,22 @@
-import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Dimensions, TouchableOpacity } from 'react-native';
 
 export default function ComponentProductFlat() {
+const numColumns = 3; // Define o número de colunas por linha
 
     const products = [
-        { image: "https://png.pngtree.com/png-vector/20240905/ourmid/pngtree-black-dslr-camera-with-large-lens-clipart-illustration-stock-photo-png-image_13758787.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" },
-        { image: "https://png.pngtree.com/png-clipart/20250104/original/pngtree-trendy-smartwatch-with-notifications-and-apps-png-image_18812139.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" },
-        { image: "https://png.pngtree.com/png-clipart/20241210/original/pngtree-nike-shoes-transparent-png-image_17778783.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" },
-        { image: "https://png.pngtree.com/png-vector/20230902/ourmid/pngtree-traveling-suitcase-luggage-png-image_9687747.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" },
-        { image: "https://png.pngtree.com/png-clipart/20241114/original/pngtree-blue-wireless-gaming-headset-png-image_17009772.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" },
-        { image: "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png", textoPrincipal: "Câmera Mirrorless Sony Alpha", textoSecundario: "R$9.800,00" }
+        { image: "https://png.pngtree.com/png-vector/20240905/ourmid/pngtree-black-dslr-camera-with-large-lens-clipart-illustration-stock-photo-png-image_13758787.png", textoPrincipal: "Pizza de Frango", textoSecundario: "R$40,00" },
+        { image: "https://png.pngtree.com/png-clipart/20250104/original/pngtree-trendy-smartwatch-with-notifications-and-apps-png-image_18812139.png", textoPrincipal: "Pizza de 4 Queijos", textoSecundario: "R$40,00" },
+        { image: "https://png.pngtree.com/png-clipart/20241210/original/pngtree-nike-shoes-transparent-png-image_17778783.png", textoPrincipal: "Pizza de Calabresa", textoSecundario: "R$40,00" },
+        { image: "https://png.pngtree.com/png-vector/20230902/ourmid/pngtree-traveling-suitcase-luggage-png-image_9687747.png", textoPrincipal: "Pizza de Camarão", textoSecundario: "R$40,00" },
+        { image: "https://png.pngtree.com/png-clipart/20241114/original/pngtree-blue-wireless-gaming-headset-png-image_17009772.png", textoPrincipal: "Pizza Portuguesa", textoSecundario: "R$40,00" },
+        { image: "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png", textoPrincipal: "Pizza de Lombo", textoSecundario: "R$40,00" }
 
     ]
     return (
         <View style={styles.container}>
 
             <FlatList
+          
                 data={products}
                 renderItem={({ item }) => <ProdutosLista data={item} />}
             />
@@ -25,25 +27,31 @@ export default function ComponentProductFlat() {
 
 function ProdutosLista({ data }) {
     return (
-        <View style={styles.containerProduct}>
 
-            <View style={styles.imageProduct}>
-                <Image
-                    source={{ uri: data.image }}
-                    style={{ width: 80, height: 80 }}
-                    resizeMode='contain'
+        <View style={styles.container}>
 
-                />
-            </View>
-            <View style={styles.textContainer}>
-                <text style={styles.texto1}>{data.textoPrincipal}</text>
+            <View style={styles.containerProduct}>
 
-                <View style={styles.carrinhoContainer}>
-                <text style={styles.texto2}>{data.textoSecundario}</text>
-    
-                <TouchableOpacity>
-                        <Text style={styles.texto3}>➕🛒 </Text>
-                </TouchableOpacity>
+                <View style={styles.imageProduct}>
+                    <Image
+                        source={{ uri: data.image }}
+                        style={{ width: 80, height: 80 }}
+                        resizeMode='contain'
+
+                    />
+                </View>
+
+                <View style={styles.textContainer}>
+                    <text style={styles.texto1}>{data.textoPrincipal}</text>
+
+                    <View style={styles.carrinhoContainer}>
+                        <text style={styles.texto2}>{data.textoSecundario}</text>
+
+                        <TouchableOpacity>
+                            <Text style={styles.texto3}>➕🛒 </Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
             </View>
 
@@ -52,43 +60,34 @@ function ProdutosLista({ data }) {
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-
     },
+
 
     containerProduct: {
+        flexDirection: "row",
+        flexWrap: 'wrap',
         backgroundColor: "white",
-        width: 400,
+        width: 220,
         height: 100,
         borderRadius: 12,
-        alignItems: 'center',
-        flexDirection: "row",
-        margin: 10
+        margin: 10,
+        justifyContent: 'center',
+        alignContent: 'center'
     },
 
-    imageProduct: {
-        height: 85,
-        width: 100,
-        marginLeft: 10,
-        borderRadius: 12,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+
 
     textContainer: {
         marginLeft: 10,
-        width: 270,
+        width: '50%',
         height: 80,
     },
 
     carrinhoContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-around',
-        gap: 130
     },
 
     texto1: {
@@ -99,6 +98,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: "#017ff5"
     },
+
     texto3: {
         fontSize: 15,
     },
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, FlatList, FlatListComponent, Button, TouchableOpacity } from 'react-native';
-import { Picker, ScrollView } from 'react-native-web';
+import { ImageBackground, Picker, ScrollView } from 'react-native-web';
 import { useNavigation } from '@react-navigation/native';
 import ComponentPicker from '../../components/ComponentPicker';
 import ComponentStorage from '../../components/ComponentStorage';
@@ -12,105 +12,170 @@ export default function Home() {
 
     return (
         <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
 
-            <ScrollView>
-                <View style={styles.container2}>
+                <View style={styles.testeView}>
+                    <View style={styles.botao}><Text onPress={() => navigation.navigate('Home')}>Home</Text></View>
+                    <View style={styles.botao}><Text onPress={() => navigation.navigate('Sobre')}>Sobre</Text></View>
+                    <View style={styles.botao}><Text onPress={() => navigation.navigate('Pedido')}>Pedido</Text></View>
+                    <View style={styles.botao}><Text onPress={() => navigation.navigate('Contato')}>Contato</Text></View>
+                </View>
 
-                    <View style={styles.testeView}>
-                        <View style={styles.botao}><Text title="Ir para página home" onPress={() => navigation.navigate('Home')}>Home</Text></View>
-                        <View style={styles.botao}><Text title="Ir para página sobre" onPress={() => navigation.navigate('Sobre')}>Sobre</Text></View>
-                        <View style={styles.botao}><Text title="Ir para página pedidos" onPress={() => navigation.navigate('Pedido')}>Pedido</Text></View>
-                        <View style={styles.botao}><Text title="Ir para página pedidos" onPress={() => navigation.navigate('Contato')}>Contato</Text></View>
-                    </View>
+                <View style={styles.textoImagem}>
+                    <Text style={styles.text}>Temos a pizza mais gostosa do Rio de Janeiro!</Text>
+                </View>
 
-                    <View style={styles.testeView2}>
-                        <Text style={styles.text}> Temos a pizza mais gostoso do Rio de Janeiro!</Text>
-                    </View>
+                <View style={styles.containerImagePizza}>
+                    <Image
+                        source={{ uri: "https://cms-blog.saipos.com/propaganda-delivery-de-pizza-SAIPOS-sistema-para-restaurante.png" }}
+                        style={styles.imagePizza}
+                        resizeMode="cover"
+                    />
+                </View>
 
-                    <View style={styles.containerPizza}>
-                        <Image
-                            source={{ uri: "https://cms-blog.saipos.com/propaganda-delivery-de-pizza-SAIPOS-sistema-para-restaurante.png" }}
-                            style={{ width: '100%', height: '100%' }}
-                        />
-                    </View>
+                <View style={styles.textoImagem}>
+                    <Text style={styles.text}>Se delicie com nossos incríveis sabores de Pizza!</Text>
+                </View>
 
-                    <View style={styles.testeView2}>
-                        <Text style={styles.text}> Se delicie com nossos incríveis sabores de Pizza!</Text>
-                    </View>
+                <View style={styles.promocoes}>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}> <Image
+                            source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwapaULs9zZ6c36uw1pkhxfYyOIgqtS9MPi26e1SvNXlPaMZRYDERKVYU&s=10" }}
+                            style={styles.imagePromocoes}
+                            resizeMode="cover"
+                        /></View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}>
+                            <Image
+                                source={{ uri: "https://i.pinimg.com/1200x/18/75/80/1875805ab9b653ed182f6c804458b9a2.jpg" }}
+                                style={styles.imagePromocoes}
+                                resizeMode="cover"
+                            />
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}>
+                            <Image
+                                source={{ uri: "https://www.pizzastorvelli.site/assets/img/produto-destaque.jpg" }}
+                                style={styles.imagePromocoes}
+                                resizeMode="cover"
+                            />
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}> <Image
+                            source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1SLVxQu5ejVhqmgdN20hXqqb1wZiTpLNE3FpmxGORfxQi9dJX0o6fz6A&s=10" }}
+                            style={styles.imagePromocoes}
+                            resizeMode="cover"
+                        /></View>
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}> <Image
+                            source={{ uri: "https://i.pinimg.com/736x/04/14/65/0414652b5a1c6ad22ede686db9113e4a.jpg" }}
+                            style={styles.imagePromocoes}
+                            resizeMode="cover"
+                        /></View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate('Pedido')}>
+                        <View style={styles.viewPromocoes}> <Image
+                            source={{ uri: "https://img.magnific.com/psd-premium/midia-social-para-pizzaria-midia-social-para-pizzeria_601378-142.jpg?semt=ais_hybrid&w=740&q=80" }}
+                            style={styles.imagePromocoes}
+                            resizeMode="cover"
+                        /></View>
+                    </TouchableOpacity>
 
                 </View>
 
-            </ScrollView>
-        </View>
+            </ScrollView >
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'gray',
         flex: 1,
+        backgroundColor: 'gray',
+    },
+
+    scrollContainer: {
+        width: '100%',
         alignItems: 'center',
-        justifyContent: 'center',
     },
 
-    container2: {
+    testeView: {
+        width: '100%',
+        backgroundColor: '#1b1b19',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        paddingVertical: 5,
+    },
+
+    textoImagem: {
+        width: '100%',
+        backgroundColor: '#ff9900',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-
-    containerPizza: {
-        width: 700,
-        height: 500,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    buttonPizza: {
-        backgroundColor: '#00f7ff',
-        borderRadius: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 200,
-        height: 80
-    },
-
-    textButtonPizza: {
-        fontSize: 20,
-        textAlign: 'center',
     },
 
     text: {
-        fontSize: 25,
-        borderRadius: 8,
+        fontSize: 15,
         textAlign: 'center',
+        paddingHorizontal: 15,
     },
 
-
-    testeView: {
-        backgroundColor: '#1b1b19',
+    containerImagePizza: {
         width: '100%',
-        height: 60,
-        flexDirection: 'row',
-        textAlign: 'center',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
+        backgroundColor: '#fff',
     },
 
-    testeView2: {
-        backgroundColor: '#ff9900',
+    imagePizza: {
         width: '100%',
-        height: 40,
-        textAlign: 'center',
-        justifyContent: 'center'
+        aspectRatio: 16 / 9,
     },
 
     botao: {
-        backgroundColor: '#ffa600', // Cor de fundo azul
-        paddingVertical: 12,       // Espaçamento interno vertical
-        paddingHorizontal: 24,     // Espaçamento interno horizontal
-        borderRadius: 8,           // Cantos arredondados
-        elevation: 3,              // Sombra leve para Android
+        backgroundColor: '#ffa600',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 8,
+        margin: 5,
+        elevation: 3,
         shadowOpacity: 0.25,
         shadowRadius: 6,
+    },
+
+    promocoes: {
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        width: '100%',
+        flexDirection: 'row',
+        padding: 10,
+        backgroundColor: 'black'
+    },
+
+    viewPromocoes: {
+        width: 200,
+        height: 200,
+        marginHorizontal: 10,
+        marginVertical: 5,
+        borderRadius: 60,
+        backgroundColor: 'yellow'
+    },
+
+    imagePromocoes: {
+        width: '100%',
+        height: '100%',
+        aspectRatio: 16 / 9,
+        borderRadius: 12
     },
 });
